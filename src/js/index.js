e9651be3ef246960bfe658b91e33e622958482a5
Akram Kadri <ak224hz@student.lnu.se>
2022-07-10 08:51:03 +0100

    Refactor main index.js file and remove code duplication

--- src/js/index.js
+++ src/js/index.js
@@ -11,70 +11,35 @@ import './components/app-window'
 import './components/chat-app'
 import './components/translator-app'
 
-const liveAppInstances = []
+let liveSubAppInstances = 0
 
 const BottomPanel = document.createElement('bottom-panel')
 document.body.appendChild(BottomPanel)
 
-let activeMemoryGames = 0
-let activeChatApps = 0
-let activeTranslatorApps = 0
-
 document.addEventListener('memoryGameIconClicked', () => {
-  addMemoryApp()
+  addSubApp('memory-game')
+  liveSubAppInstances++
 })
 
 document.addEventListener('chatAppIconClicked', () => {
-  addChatApp()
+  addSubApp('chat-app')
+  liveSubAppInstances++
 })
 
 document.addEventListener('translatorAppIconClicked', () => {
-  addTranslatorApp()
+  addSubApp('translator-app')
+  liveSubAppInstances++
 })
 
 /**
- * Opens up a chat-app instance.
+ * Adds a sub-app to the main application.
  */
-const addChatApp = () => {
+const addSubApp = (componentName) => {
   const appWindow = document.createElement('app-window')
   appWindow.style.position = 'absolute'
-  appWindow.style.top = `${20 + activeChatApps * 10}px`
-  appWindow.style.left = `${activeChatApps * 10}px`
-  const chatApp = document.createElement('chat-app')
-  appWindow.shadowRoot.querySelector('.root').appendChild(chatApp)
+  appWindow.style.top = `${20 + liveSubAppInstances * 10}px`
+  appWindow.style.left = `${liveSubAppInstances * 10}px`
+  const subApp = document.createElement(componentName)
+  appWindow.shadowRoot.querySelector('.root').appendChild(subApp)
   document.querySelector('main').appendChild(appWindow)
-  liveAppInstances.push(appWindow)
-  activeChatApps++
 }
-
-/**
- * Opens up a memory-game instance.
- */
-const addMemoryApp = () => {
-  const appWindow = document.createElement('app-window')
-  appWindow.style.position = 'absolute'
-  appWindow.style.top = `${20 + activeMemoryGames * 10}px`
-  appWindow.style.left = `${activeMemoryGames * 10}px`
-  const memoryGame = document.createElement('memory-game')
-  appWindow.shadowRoot.querySelector('.root').appendChild(memoryGame)
-  document.querySelector('main').appendChild(appWindow)
-  liveAppInstances.push(appWindow)
-  activeMemoryGames++
-}
-
-/**
- * Opens up a translator-app instance.
- */
-const addTranslatorApp = () => {
-  const appWindow = document.createElement('app-window')
-  appWindow.style.position = 'absolute'
-  appWindow.style.top = `${20 + activeTranslatorApps * 10}px`
-  appWindow.style.left = `${activeTranslatorApps * 10}px`
-  const translatorApp = document.createElement('translator-app')
-  appWindow.shadowRoot.querySelector('.root').appendChild(translatorApp)
-  document.querySelector('main').appendChild(appWindow)
-  liveAppInstances.push(appWindow)
-  activeTranslatorApps++
-}
-
-addChatApp()
